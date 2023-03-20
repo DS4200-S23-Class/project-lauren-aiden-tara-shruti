5385b46f811453fe87f9e4c257fd2ebf7d9353fa
tarajoshii <119441296+tarajoshii@users.noreply.github.com>
2023-03-20 15:02:30 -0400

    brushing but not linked to bar properly

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ function build_scatter() {
 
 
   // Add brushing
-    FRAME2.call( d3.brush()                 // Add the brush feature using the d3.brush function
+    FRAME1.call( d3.brush()                 // Add the brush feature using the d3.brush function
             .extent( [ [0,0], [FRAME_WIDTH, FRAME_HEIGHT] ] ) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
             .on("start brush", updateChart) // Each time the brush selection changes, trigger the 'updateChart' function
     )
